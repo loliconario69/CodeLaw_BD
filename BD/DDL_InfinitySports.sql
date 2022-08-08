424c1bd19828ff9ccd9a2aca4c4ca7e4ce7631ec
Diego Ibañez <loliconario69@gmail.com>
2022-08-07 21:54:44 -0300

    Nuevo dll para la nueva BD

--- BD/DDL_InfinitySports.sql
+++ BD/DDL_InfinitySports.sql
@@ -3,263 +3,197 @@ CREATE DATABASE InfinitySports CHARSET utf8mb4;
 USE InfinitySports;
 
 
-CREATE TABLE `Paises` (
-  `Nombre` varchar(20) not null,
-  `Ciudad` varchar(40) not null,
-  PRIMARY KEY (`Nombre`, `Ciudad`)
+# ENTIDADES--------------------------------
+CREATE TABLE Usuario (
+  Id int unsigned auto_increment,
+  Nombre varchar(30) not null,
+  Email varchar(30),
+  Contrasenia varchar(100) not null,
+  Telefono smallint not null, # ---------------Telefono?
+  EsPremium bool not null,
+  PRIMARY KEY (Id)
+);
+CREATE TABLE Administrador ( # -------NO DEBERIA SER UNA SOLA TABLA?
+	Id int unsigned auto_increment,
+    Contraseña varchar(100) not null,
+    Tipo bool not null,
+    PRIMARY KEY (Id)
+);
+CREATE TABLE EmailAdministador (
+	IdAdministrador int unsigned,
+    Email varchar(30) not null,
+    PRIMARY KEY (IdAdministrador),
+    FOREIGN KEY (IdAdministrador) REFERENCES Administrador(Id)
+);
+CREATE TABLE Publicidad (
+	Id int unsigned auto_increment,
+	Grupo varchar(15) not null,
+    Descripcion varchar(100) not null,
+    Imagen varchar(255) not null,
+    Tipo varchar(20) not null,       #-----------¿QUE ES?-------
+    PRIMARY KEY (Id)
 );
 
-#---------------ENTIDADES------------------#
-CREATE TABLE `Publicidad` (
-  `Nombre` varchar(20) not null,
-  `URL` varchar(100) not null,
-  PRIMARY KEY (`Nombre`)
+CREATE TABLE Competencia (
+	Id int unsigned auto_increment,
+    Nombre varchar(30) not null,
+    Lugar varchar(30),
+    Tipo varchar(20) not null, #-----------Tipo?
+    Imagen varchar(255),
+    FechaInicio date not null,  #-----------------Agregé fecha inicio y fin
+    FechaFin date not null,
+    PRIMARY KEY (Id)
+);
+CREATE TABLE Etapa (
+	IdCompetencia int unsigned,
+    Nombre varchar(30) not null,
+    Lugar varchar(30) not null,
+    PRIMARY KEY (IdCompetencia,Nombre),
+    FOREIGN KEY (IdCompetencia) REFERENCES Competencia(Id)
+);
+    
+CREATE TABLE Deporte (
+	Id mediumint unsigned auto_increment,
+    Nombre varchar(30) not null,
+    Imagen varchar(255),
+    PRIMARY KEY (Id)
 );
 
-CREATE TABLE `Usuarios` (
-  `Email` varchar(25),
-  `Nombre` varchar(20) not null,
-  `Apellido` varchar(20) not null,
-  `Contrasenia` varchar(50) not null,
-  `TipoDeCuenta` ENUM ('Normal', 'Premium') not null,
-  PRIMARY KEY (`Email`)
-);
-CREATE TABLE `Administradores` (
-  `Email` varchar(25),
-  `Nombre` varchar(20) not null,
-  `Apellido` varchar(20) not null,
-  `Contrasenia` varchar(50) not null,
-  `Rol` ENUM ('Funcionario', 'Administrador') not null,
-  PRIMARY KEY (`Email`)
+CREATE TABLE Juez (
+	Id int unsigned auto_increment,
+    Nombre varchar(60) not null, # ---------Atributo compuesto?
+    PRIMARY KEY (Id)
 );
 
-CREATE TABLE `Competicion` (
-  `Id` int unsigned auto_increment,
-  `Nombre` varchar(35) not null,
-  `Etapa` varchar(30) not null,
-  `FechaInicio` datetime not null,
-  `FechaFin` datetime not null,
-  `Ubicacion` varchar(40)  not null,
-  `Conformacion` ENUM ('ParJugador', 'ParEquipo', 'VariosJugadores') not null,
-  PRIMARY KEY (`Id`)
+CREATE TABLE Encuentro (
+	Id int unsigned auto_increment,
+    Fecha datetime not null, # ---------Corrección de MR encuentro
+    Lugar varchar(30),
+    PRIMARY KEY (Id)
 );
 
-CREATE TABLE `Deporte` (
-  `Nombre` varchar(20),
-  `Descripcion` varchar(100) not null,
-  PRIMARY KEY (`Nombre`)
+CREATE TABLE Contendiente (
+	Id int unsigned auto_increment,
+    Imagen varchar(255),
+    Pais varchar(20) not null,
+    FechaDeNacimiento date,
+    PRIMARY KEY (Id)
 );
 
-CREATE TABLE `Equipo` (
-  `Id` int unsigned auto_increment,
-  `Nombre` varchar(30),
-  `Pais` varchar(15) not null,
-  `Ciudad` varchar(40) not null,
-  PRIMARY KEY (`Id`),
-  FOREIGN KEY (`Pais`, `Ciudad`) REFERENCES `Paises` (`Nombre`, `Ciudad`)
+CREATE TABLE Resultado (
+	IdContendiente int unsigned,
+	IdEncuentro int unsigned,
+    Tiempo time,
+    Puntaje mediumint,
+    PRIMARY KEY (IdContendiente,IdEncuentro),
+    FOREIGN KEY (IdContendiente) REFERENCES Contendiente(Id),
+    FOREIGN KEY (IdEncuentro) REFERENCES Encuentro(Id)
+);
+CREATE TABLE Equipo (
+	IdContendiente int unsigned,
+    Nombre varchar(30) not null,
+    PRIMARY KEY (IdContendiente),
+    FOREIGN KEY (IdContendiente) REFERENCES Contendiente(Id)
+);
+CREATE TABLE Deportista (
+	IdContendiente int unsigned,
+    PrimerNombre varchar(15) not null,
+    SegundoNombre varchar(15),
+    PrimerApellido varchar(15) not null,
+    SegundoApellido varchar(15),
+    PRIMARY KEY (IdContendiente),
+    FOREIGN KEY (IdContendiente) REFERENCES Contendiente(Id)
 );
 
-CREATE TABLE `Persona` (
-  `Id` int unsigned auto_increment,
-  `PrimerNombre` varchar(15) not null,
-  `SegundoNombre` varchar(15),
-  `PrimerApellido` varchar(15) not null,
-  `SegundoApellido` varchar(15),
-  `FechaNacimiento` date  not null,
-  `Pais` varchar(15) not null,
-  `Ciudad` varchar(40) not null,
-  PRIMARY KEY (`Id`),
-  FOREIGN KEY (`Pais`, `Ciudad`) REFERENCES `Paises` (`Nombre`, `Ciudad`)
+# RELACIONES ----------------------------------------
+
+CREATE TABLE UconsumeP (
+	IdUsuario int unsigned,
+    IdPublicidad int unsigned,
+    PRIMARY KEY (IdUsuario,IdPublicidad),
+    FOREIGN KEY (IdUsuario) REFERENCES Usuario(Id),
+    FOREIGN KEY (IdPublicidad) REFERENCES Publicidad(Id)
+);
+    
+CREATE TABLE UsuscribeD (
+	IdUsuario int unsigned,
+    IdDeportista int unsigned,
+    PRIMARY KEY (IdUsuario,IdDeportista),
+    FOREIGN KEY (IdUsuario) REFERENCES Usuario(Id),
+    FOREIGN KEY (IdDeportista) REFERENCES Deportista(IdContendiente)
+);
+CREATE TABLE UsuscribeEq (
+	IdUsuario int unsigned,
+    IdEquipo int unsigned,
+    PRIMARY KEY (IdUsuario,IdEquipo),
+    FOREIGN KEY (IdUsuario) REFERENCES Usuario(Id),
+    FOREIGN KEY (IdEquipo) REFERENCES Equipo(IdContendiente)
 );
 
-CREATE TABLE `Jugador` (
-  `Id` int unsigned,
-  PRIMARY KEY (`Id`),
-  FOREIGN KEY (`Id`) REFERENCES `Persona` (`Id`) ON DELETE CASCADE
-);
-CREATE TABLE `Juez` (
-  `Id` int unsigned,
-  PRIMARY KEY (`Id`),
-  FOREIGN KEY (`Id`) REFERENCES `Persona` (`Id`)  ON DELETE CASCADE
-);
-CREATE TABLE `Entrenador` (
-  `Id` int unsigned,
-  PRIMARY KEY (`Id`),
-  FOREIGN KEY (`Id`) REFERENCES `Persona` (`Id`)  ON DELETE CASCADE
+CREATE TABLE UsuscribeEn (
+	IdUsuario int unsigned,
+    IdEncuentro int unsigned,
+    PRIMARY KEY (IdUsuario,IdEncuentro),
+    FOREIGN KEY (IdUsuario) REFERENCES Usuario(Id),
+    FOREIGN KEY (IdEncuentro) REFERENCES Encuentro(Id)
 );
 
-#---------------RELACIONES------------------#
-
-CREATE TABLE `JugadorCompite` (
-  `IdJugador1` int unsigned,
-  `IdJugador2` int unsigned,
-  `Fecha` datetime,
-  `PuntosJugador1` varchar(10),
-  `PuntosJugador2` varchar(10),
-  PRIMARY KEY (`IdJugador1`, `IdJugador2`, `Fecha`),
-  FOREIGN KEY (`IdJugador1`) REFERENCES `Jugador` (`Id`),
-  FOREIGN KEY (`IdJugador2`) REFERENCES `Jugador` (`Id`)
-);
-CREATE TABLE `EquipoCompite` (
-  `IdEquipo1` int unsigned,
-  `IdEquipo2` int unsigned,
-  `Fecha` datetime,
-  `PuntosEquipo1` varchar(10),
-  `PuntosEquipo2` varchar(10),
-  PRIMARY KEY (`IdEquipo1`, `IdEquipo2`, `Fecha`),
-  FOREIGN KEY (`IdEquipo1`) REFERENCES `Equipo` (`Id`),
-  FOREIGN KEY (`IdEquipo2`) REFERENCES `Equipo` (`Id`)
+CREATE TABLE UsuscribeC (
+	IdUsuario int unsigned,
+    IdCompetencia int unsigned,
+    PRIMARY KEY (IdUsuario,IdCompetencia),
+    FOREIGN KEY (IdUsuario) REFERENCES Usuario(Id),
+    FOREIGN KEY (IdCompetencia) REFERENCES Competencia(Id)
 );
 
-CREATE TABLE `SeSuscribeParJugador` (
-  `EmailUsuario` varchar(25),
-  `IdJugador1` int unsigned,
-  `IdJugador2` int unsigned,
-  `Fecha` datetime,
-  PRIMARY KEY (`EmailUsuario`, `IdJugador1`, `IdJugador2`, `Fecha`),
-  FOREIGN KEY (`EmailUsuario`) REFERENCES `Usuarios` (`Email`) ON DELETE CASCADE,
-  FOREIGN KEY (`IdJugador1`,`IdJugador2`,`Fecha`) REFERENCES `jugadorcompite` (`IdJugador1`,`IdJugador2`,`Fecha`)
-);
-CREATE TABLE `SeSuscribeParEquipo` (
-  `EmailUsuario` varchar(25),
-  `IdEquipo1` int unsigned,
-  `IdEquipo2` int unsigned,
-  `Fecha` datetime,
-  PRIMARY KEY (`EmailUsuario`, `IdEquipo1`, `IdEquipo2`, `Fecha`),
-  FOREIGN KEY (`EmailUsuario`) REFERENCES `Usuarios` (`Email`) ON DELETE CASCADE,
-  FOREIGN KEY (`IdEquipo1`,`IdEquipo2`,`Fecha`) REFERENCES `equipocompite` (`IdEquipo1`,`IdEquipo2`,`Fecha`)
-);
-CREATE TABLE `SeSuscribeVariosJugadores` (
-  `EmailUsuario` varchar(25),
-  `IdComp` int unsigned,
-  PRIMARY KEY (`EmailUsuario`, `IdComp`),
-  FOREIGN KEY (`EmailUsuario`) REFERENCES `Usuarios` (`Email`) ON DELETE CASCADE,
-  FOREIGN KEY (`IdComp`) REFERENCES `Competicion` (`Id`)
+CREATE TABLE SePracticaDeporte (
+	IdCompetencia int unsigned,
+    Nombre varchar(30),
+    IdDeporte mediumint unsigned,
+    PRIMARY KEY (IdCompetencia,Nombre,IdDeporte),
+    FOREIGN KEY (IdCompetencia,Nombre) REFERENCES Etapa(IdCompetencia,Nombre),
+    FOREIGN KEY (IdDeporte) REFERENCES Deporte(Id)
 );
 
-CREATE TABLE `PersonaPertenece` (
-  `IdPersona` int unsigned,
-  `Deporte` varchar(20),
-  PRIMARY KEY (`IdPersona`, `Deporte`),
-  FOREIGN KEY (`IdPersona`) REFERENCES `Persona` (`Id`) ON DELETE CASCADE,
-  FOREIGN KEY (`Deporte`) REFERENCES `Deporte` (`Nombre`) ON DELETE CASCADE
-);
-CREATE TABLE `Comp_de_deport` (
-  `Deporte` varchar(20),
-  `IdComp` int unsigned,
-  PRIMARY KEY (`Deporte`, `IdComp`),
-  FOREIGN KEY (`Deporte`) REFERENCES `Deporte` (`Nombre`) ON DELETE CASCADE,
-  FOREIGN KEY (`IdComp`) REFERENCES `Competicion` (`Id`) ON DELETE CASCADE
-);
-CREATE TABLE `EquipoPertenece` (
-  `IdEquipo` int unsigned,
-  `Deporte` varchar(20),
-  PRIMARY KEY (`IdEquipo`, `Deporte`),
-  FOREIGN KEY (`IdEquipo`) REFERENCES `Equipo` (`Id`) ON DELETE CASCADE,
-  FOREIGN KEY (`Deporte`) REFERENCES `Deporte` (`Nombre`) ON DELETE CASCADE
+CREATE TABLE EcontieneE (
+	IdCompetencia int unsigned,
+    NombreEtapa varchar(30),
+    IdEncuentro int unsigned,
+    PRIMARY KEY (IdCompetencia,NombreEtapa,IdEncuentro),
+    FOREIGN KEY (IdCompetencia,NombreEtapa) REFERENCES Etapa(IdCompetencia,Nombre),
+    FOREIGN KEY (IdEncuentro) REFERENCES Encuentro(Id)
 );
 
-CREATE TABLE `J_pertenece_E` (
-  `IdJugador` int unsigned,
-  `IdEquipo` int unsigned,
-  `posicion` varchar(20),
-  PRIMARY KEY (`IdJugador`, `IdEquipo`),
-  FOREIGN KEY (`IdJugador`) REFERENCES `Jugador` (`Id`),
-  FOREIGN KEY (`IdEquipo`) REFERENCES `Equipo` (`Id`) ON DELETE CASCADE
+CREATE TABLE EtieneJ (
+	IdEncuentro int unsigned,
+    IdJuez int unsigned,
+    PRIMARY KEY (IdEncuentro,IdJuez),
+    FOREIGN KEY (IdEncuentro) REFERENCES Encuentro(Id),
+    FOREIGN KEY (IdJuez) REFERENCES Juez(Id)
 );
 
-CREATE TABLE `Juez_ParJugador` (
-  `IdJugador1` int unsigned,
-  `IdJugador2` int unsigned,
-  `Fecha` datetime,
-  `IdJuez` int unsigned,
-  PRIMARY KEY (`IdJugador1`, `IdJugador2`, `Fecha`, `IdJuez`),
-  FOREIGN KEY (`IdJugador1`,`IdJugador2`,`Fecha`) REFERENCES `jugadorcompite` (`IdJugador1`,`IdJugador2`,`Fecha`),
-  FOREIGN KEY (`IdJuez`) REFERENCES `Juez` (`Id`) ON DELETE CASCADE
-  );
-CREATE TABLE `Juez_ParEquipo` (
-  `IdEquipo1` int unsigned,
-  `IdEquipo2` int unsigned,
-  `Fecha` datetime,
-  `IdJuez` int unsigned,
-  PRIMARY KEY (`IdEquipo1`, `IdEquipo2`, `Fecha`, `IdJuez`),
-  FOREIGN KEY (`IdEquipo1`,`IdEquipo2`,`Fecha`) REFERENCES `equipocompite` (`IdEquipo1`,`IdEquipo2`,`Fecha`),
-  FOREIGN KEY (`IdJuez`) REFERENCES `Juez` (`Id`) ON DELETE CASCADE
-  );
-CREATE TABLE `Juez_VariosJugadores` (
-  `IdJuez` int unsigned,
-  `IdComp` int unsigned,
-  PRIMARY KEY (`IdJuez`, `IdComp`),
-  FOREIGN KEY (`IdJuez`) REFERENCES `Juez` (`Id`),
-  FOREIGN KEY (`IdComp`) REFERENCES `Competicion` (`Id`) ON DELETE CASCADE
-);  
-
-CREATE TABLE `Entrenador_Jugador` (
-  `IdJugador` int unsigned,
-  `IdEntrenador` int unsigned,
-  PRIMARY KEY (`IdJugador`, `IdEntrenador`),
-  FOREIGN KEY (`IdJugador`) REFERENCES `Jugador` (`Id`),
-  FOREIGN KEY (`IdEntrenador`) REFERENCES `Entrenador` (`Id`) ON DELETE CASCADE
-);
-CREATE TABLE `Entrenador_Equipo` (
-  `IdEquipo` int unsigned,
-  `IdEntrenador` int unsigned,
-  PRIMARY KEY (`IdEquipo`, `IdEntrenador`),
-  FOREIGN KEY (`IdEquipo`) REFERENCES `Equipo` (`Id`),
-  FOREIGN KEY (`IdEntrenador`) REFERENCES `Entrenador` (`Id`) ON DELETE CASCADE
-); 
-
-CREATE TABLE `JugadoresParticipan` (
-  `IdJugador1` int unsigned,
-  `IdJugador2` int unsigned,
-  `IdComp` int unsigned,
-  PRIMARY KEY (`IdJugador1`, `IdJugador2`, `IdComp`),
-  FOREIGN KEY (`IdJugador1`) REFERENCES `Jugador` (`Id`),
-  FOREIGN KEY (`IdJugador2`) REFERENCES `Jugador` (`Id`),
-  FOREIGN KEY (`IdComp`) REFERENCES `Competicion` (`Id`) ON DELETE CASCADE
-);
-CREATE TABLE `JugadorParticipa` (
-  `IdJugador` int unsigned,
-  `IdComp` int unsigned,
-  `Puntos` varchar(10),
-  PRIMARY KEY (`IdJugador`, `IdComp`),
-  FOREIGN KEY (`IdJugador`) REFERENCES `Jugador` (`Id`),
-  FOREIGN KEY (`IdComp`) REFERENCES `Competicion` (`Id`) ON DELETE CASCADE
-);
-CREATE TABLE `EquiposParticipan` (
-  `IdEquipo1` int unsigned,
-  `IdEquipo2` int unsigned,
-  `IdComp` int unsigned,
-  PRIMARY KEY (`IdEquipo1`, `IdEquipo2`, `IdComp`),
-  FOREIGN KEY (`IdEquipo1`) REFERENCES `Equipo` (`Id`),
-  FOREIGN KEY (`IdEquipo2`) REFERENCES `Equipo` (`Id`),
-  FOREIGN KEY (`IdComp`) REFERENCES `Competicion` (`Id`) ON DELETE CASCADE
+CREATE TABLE CparticipaE (
+	IdContendiente int unsigned,
+    IdEncuentro int unsigned,
+    PRIMARY KEY (IdContendiente,IdEncuentro),
+    FOREIGN KEY (IdContendiente) REFERENCES Contendiente(Id),
+    FOREIGN KEY (IdEncuentro) REFERENCES Encuentro(Id)
 );
 
 
 
-DELIMITER $$
-CREATE TRIGGER comp_de_deport_BorrarCompetencia
-AFTER DELETE ON comp_de_deport
-FOR EACH ROW
-BEGIN
-	DELETE FROM competicion WHERE
-		Id = OLD.idComp;
-END$$
 
-CREATE TRIGGER PersonaPertenece_BorrarJugador
-AFTER DELETE ON personapertenece
-FOR EACH ROW
-BEGIN
-	DELETE FROM persona WHERE
-		Id = OLD.IdPersona;
-END$$
 
-CREATE TRIGGER EquipoPertenece_BorrarEquipo
-AFTER DELETE ON equipopertenece
-FOR EACH ROW
-BEGIN
-	DELETE FROM equipo WHERE
-		Id = OLD.IdEquipo;
-END$$
-DELIMITER ;
+
+
+
+
+
+
+
+
+
+
+
+
+
