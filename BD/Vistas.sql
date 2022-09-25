9b47321b1fdf3c041c12beeab0399b031ecf5dbb
diego <loliconario69@gmail.cm>
2022-09-24 22:49:51 -0300

    MysqlDump actualizado con las vistas

--- BD/Vistas.sql
+++ BD/Vistas.sql
@@ -1,19 +1,3 @@
-/*
-SELECT 
-    E.IdCompetencia as IdCompetencia,
-    E.Nombre as Nombre,
-    E.Lugar as Lugar,
-    D.Nombre as Deporte,
-    D.Id as IdDeporte
-FROM
-    Etapa E
-        INNER JOIN
-    sepracticadeporte se ON E.IdCompetencia = se.IdCompetencia AND E.Nombre = se.Nombre
-        INNER JOIN
-    Deporte D ON E.IdCompetencia = @Id AND E.Nombre = @Nombre AND D.Id = se.IdDeporte
-
-
-*/
 CREATE VIEW vista_DetallesDeEtapa AS
 
 SELECT 
@@ -28,42 +12,10 @@ FROM
     sepracticadeporte se ON E.IdCompetencia = se.IdCompetencia AND E.Nombre = se.Nombre
         INNER JOIN
     Deporte D ON D.Id = se.IdDeporte;
-    
-    
-    #-----------------------------------------------------------------
-    #-----------------------------------------------------------------
-    #-----------------------------------------------------------------
-    #-----------------------------------------------------------------
-    #-----------------------------------------------------------------
-    #-----------------------------------------------------------------
-    #-----------------------------------------------------------------
-    #-----------------------------------------------------------------
-    #-----------------------------------------------------------------
-    #-----------------------------------------------------------------
-    
-/*
-SELECT 
-    Eq.Id AS EquipoId,
-    Eq.Nombre AS EquipoNombre,
-    D.Id AS DeportistaId,
-    CONCAT(D.PrimerNombre, ' ', COALESCE(D.SegundoNombre, ''), ' ', D.PrimerApellido, ' ', COALESCE(D.SegundoApellido, '')) AS DeportistaNombre,
-    CONCAT(COALESCE(P.Puntaje, ''), COALESCE(P.Tiempo, '')) AS Anotacion,
-    EEn.Puntaje
-FROM
-    EcontieneE EE
-        INNER JOIN
-    Encuentro En ON En.Id = EE.IdEncuentro AND EE.IdCompetencia = @IdCompetencia AND EE.NombreEtapa = @NombreEtapa AND En.Id = @IdEncuentro
-        INNER JOIN
-    ParticipaEn P ON P.IdEncuentro = En.Id AND P.IdEquipo = @IdEquipo AND P.IdDeportista = @IdDeportista
-        INNER JOIN
-    Equipo Eq ON Eq.Id = P.IdEquipo
-        INNER JOIN
-    Deportista D ON D.Id = P.IdDeportista
-        LEFT JOIN
-    eparticipaen EEn ON EEn.IdEncuentro = En.Id AND P.IdEquipo = EEn.IdEquipo;
-    */
+
  
 CREATE VIEW vista_DetallesEncuentro AS
+
 SELECT 
 	EE.IdCompetencia AS IdCompetencia,
     EE.NombreEtapa AS NombreEtapa,
